40eb1e4cc4f07b4ce71e764193e246fa071a8bb8
sfideunivr <sfideunivr@libero.it>
2017-04-20 09:30:48 +0200

    Add files via upload

--- arena/Tron/src/it/univr/SfideDiProgrammazione/Tron/Giocatore2.java
+++ arena/Tron/src/it/univr/SfideDiProgrammazione/Tron/Giocatore2.java
@@ -1,6 +1,8 @@
 package it.univr.SfideDiProgrammazione.Tron;
 
+import java.util.ArrayList;
 import java.util.Random;
+import java.lang.Math;
 
 public class Giocatore2 extends Giocatore {
 
@@ -8,32 +10,121 @@ public class Giocatore2 extends Giocatore {
 		super(nomeGiocatore, i, j);
 		// TODO Auto-generated constructor stub
 	}
-
-//	@Override
-//	protected char scegliMossaDaFare(int iAvversario, int jAvversario, char[][] scacchiera) {
-//		System.out.println("Giocatore 2 mossa effettuata.");
-//		return 'U';
-//	
-//
-//	}
+	
+	/*
+	Fa una mossa casuale.
 	
 	@Override
-	protected char scegliMossaDaFare(int iAvversario, int jAvversario, char[][] scacchiera) {
-		// Se la posizione a sinistra esiste ed è libera va a sinistra.
-		if ((this.getY() != 0) && (scacchiera[this.getX()][this.getY()-1] == '0')){
+	protected char scegliMossaDaFare(int posXavv, int posYavv, char[][] scacchiera) {
+		int scelta = new Random().nextInt(3);
+		
+		switch(scelta) {
+			case 0: return 'L';
+			case 1: return 'R';
+			case 2: return 'U';
+			case 3: return 'D';
+			default: return 'L';
+		}
+
+	}
+	*/
+	
+	/*
+	Fa una mossa casuale, ma controlla se la casella è libera.
+	
+	@Override
+	protected char scegliMossaDaFare(int posXavv, int posYavv, char[][] scacchiera) {
+		boolean cicla = true;
+		char mossa = 'L';
+		int i = 0;
+		
+		while(cicla && i < 10) {
+			int scelta = new Random().nextInt(3);
+			
+			switch(scelta) {
+				case 0: if(liberaASinistra(scacchiera)) mossa = 'L'; cicla = false; break;
+				case 1: if(liberaADestra(scacchiera)) mossa = 'R'; cicla = false; break;
+				case 2: if(liberaInAlto(scacchiera)) mossa = 'U'; cicla = false; break;
+				case 3: if(liberaInBasso(scacchiera)) mossa = 'D'; cicla = false; break;
+			}
+			i++;
+		}
+		
+		return mossa;
+	}
+	*/
+	
+	/*
+	Cerca di riempire tutta la scacchiera.
+	*/
+	@Override
+	protected char scegliMossaDaFare(int posXavv, int posYavv, char[][] scacchiera) {
+		
+		if(liberaASinistra(scacchiera))
 			return 'L';
-		}
-		// Se la posizione a sinistra non è libera o non esiste vai su se esiste ed è libera.
-		else if (this.getX() != 0 && scacchiera[this.getX()-1][this.getY()] == '0'){
+		else if(liberaInAlto(scacchiera))
 			return 'U';
-		}
-		// Se la posizione in alto non è libera o non esiste vai a destra se esiste ed è libera.
-		else if (this.getY() != 7 && scacchiera[this.getX()][this.getY()+1] == '0'){
+		else if(liberaADestra(scacchiera))
 			return 'R';
-		}
-		// Altrimenti se non posso andare né a sinistra, né in alto, né a destra posso andare solo in basso.
 		else
 			return 'D';
+
+	}
+	
+	private boolean liberaASinistra(char[][] scacchiera) {
+		return posY != 0 && scacchiera[posX][posY - 1] == '0';
+	}
+	
+	private boolean liberaADestra(char[][] scacchiera) {
+		return posY != DIM - 1 && scacchiera[posX][posY + 1] == '0';
+	}
+	
+	private boolean liberaInAlto(char[][] scacchiera) {
+		return posX != 0 && scacchiera[posX - 1][posY] == '0';
+	}
+	
+	private boolean liberaInBasso(char[][] scacchiera) {
+		return posX != DIM - 1 && scacchiera[posX + 1][posY] == '0';
+	}
+	
+	private boolean eInAltoASinistra(int posX, int posY) {
+		return (posX >= 0 && posX < DIM / 2) && (posY >= 0 && posY < DIM / 2);
+	}
+	
+	private boolean eInAltoADestra(int posX, int posY) {
+		return (posX >= 0 && posX < DIM / 2) && (posY >= DIM / 2 && posY < DIM);
+	}
+	
+	private boolean eInBassoASinistra(int posX, int posY) {
+		return (posX >= DIM / 2 && posX < DIM) && (posY >= 0 && posY < DIM / 2);
+	}
+	
+	private boolean eInBassoADestra(int posX, int posY) {
+		return (posX >= DIM / 2 && posX < DIM) && (posY >= DIM / 2 && posY < DIM);
+	}
+	
+	private boolean ePiuInAltoDellAvversario(int posXavv) {
+		return posX < posXavv;
+	}
+	
+	private boolean eInAltoQuantoLAvversario(int posXavv) {
+		return posX == posXavv;
+	}
+	
+	private boolean ePiuASinistraDellAvversario(int posYavv) {
+		return posY < posYavv;
+	}
+	
+	private boolean eASinistraQuantoLAvversario(int posYavv) {
+		return posY == posYavv;
+	}
+	
+	private int distanzaOrizzontale(int posYavv) {
+		return Math.abs(posYavv - posY);
+	}
+	
+	private int distanzaVerticale(int posXavv) {
+		return Math.abs(posXavv - posX);
 	}
 
 }
